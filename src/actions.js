fbde886e173e21cc57c9a80806cf1cd7c03f7451
DixonOrtiz <dixon.ortizc@gmail.com>
2020-02-04 13:31:12 -0300

    actions and action creators

---
+++ src/actions.js
@@ -0,0 +1,21 @@
+//some constants
+export const CHANGE_POSITION = "CHANGE_POSITION";
+export const CHANGE_COACH = "CHANGE_COACH";
+export const ADD_PLAYER = "ADD_PLAYER";
+export const DELETE_PLAYER = "DELETE_PLAYER";
+
+export function changePosition(position) {
+  return { type: CHANGE_POSITION, position };
+}
+
+export function changeCoach(coachName) {
+  return { type: CHANGE_COACH, coachName };
+}
+
+export function addPlayer(playerName) {
+  return { type: ADD_PLAYER, playerName };
+}
+
+export function deletePlayer(playerIndex) {
+  return { type: DELETE_PLAYER, playerIndex };
+}
